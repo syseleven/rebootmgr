6de1c6e530b5c6121d90a1605ee323ddd6580468
Steffen Neubauer <stefreak@googlemail.com>
2018-11-13 22:15:04 +0100

    Update tests/test_triggers.py

--- tests/test_triggers.py
+++ tests/test_triggers.py
@@ -5,7 +5,7 @@ import pytest
 from rebootmgr.main import cli as rebootmgr
 
 
-def test_reboot_trigger_not_required(run_cli, forward_port, consul_cluster, reboot_task, mocker):
+def test_reboot_not_required(run_cli, forward_port, consul_cluster, reboot_task, mocker):
     forward_port.consul(consul_cluster[0])
 
     result = run_cli(rebootmgr, ["-v", "--check-triggers"])
