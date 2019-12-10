08479a7ebe96e5e07938857ab5970e4e510f98b9
Olaf Seibert <o.seibert@syseleven.de>
2019-12-10 17:14:08 +0100

    Update version number to 0.0.22.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if not sys.version_info >= (3, 5):
 
 setup(
     name="rebootmgr",
-    version="0.0.21",
+    version="0.0.22",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
