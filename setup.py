83b605ae47142d0e4fa416ce306ca66477179f8e
Olaf Seibert <o.seibert@syseleven.de>
2020-03-18 11:09:48 +0100

    Bump version to 0.0.23.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if not sys.version_info >= (3, 5):
 
 setup(
     name="rebootmgr",
-    version="0.0.22",
+    version="0.0.23",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
