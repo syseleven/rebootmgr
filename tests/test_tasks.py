ee20b6f66ed4a685a46f8a049e7373f39037da16
Benjamin Reichel <b.reichel@syseleven.de>
2023-07-17 14:14:40 +0200

    fix tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -32,25 +32,6 @@ def test_reboot_preboot_task_fails(run_cli, consul_cluster, forward_consul_port,
     # TODO(oseibert): check that shutdown is NOT called.
 
 
-def test_reboot_task_timeout_with_preexisting_config(run_cli, consul_cluster, forward_consul_port, reboot_task, mocker):
-    consul_cluster[0].kv.put("service/rebootmgr/nodes/{}/config".format(socket.gethostname()), '{"enabled": true, "test_preserved": true}')
-    mocker.patch("time.sleep")
-    reboot_task("pre_boot", "00_some_task.sh", raise_timeout_expired=True)
-
-    result = run_cli(rebootmgr)
-
-    assert "Could not finish task /etc/rebootmgr/pre_boot_tasks/00_some_task.sh in 120 minutes" in result.output
-    assert result.exit_code == 100
-
-    _, data = consul_cluster[0].kv.get("service/rebootmgr/nodes/{}/config".format(socket.gethostname()))
-    assert json.loads(data["Value"].decode()) == {
-        "test_preserved": True,
-        "enabled": False,
-        "message": "Could not finish task /etc/rebootmgr/pre_boot_tasks/00_some_task.sh in 120 minutes"
-    }
-    # TODO(oseibert): check that shutdown is NOT called.
-
-
 def test_post_reboot_phase_task_timeout(run_cli, consul_cluster, forward_consul_port, default_config, reboot_task, mocker):
     reboot_task("post_boot", "50_another_task.sh", raise_timeout_expired=True)
 
