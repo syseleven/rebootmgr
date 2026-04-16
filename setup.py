f218b1afcff80306afb1d74d30381354330b7aa4
Morteza Bashsiz <mbashsiz@syseleven.de>
2026-04-16 08:00:51 +0200

    Exclude other groups from consul health check members

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if not sys.version_info >= (3, 5):
 
 setup(
     name="rebootmgr",
-    version="0.0.31",
+    version="0.0.32",
     packages=find_packages(),
     maintainer="Syseleven Cloudstackers",
     maintainer_email="cloudstackers@syseleven.de",
